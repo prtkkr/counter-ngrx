fab28ec980564cef14e4e80f8117f2c7d79be175
Pratik Kumar <pratik.kumar@mobileprogramming.com>
2023-06-22 16:39:19 +0530

    basic counter setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CounterComponent } from './counter/counter/counter.component';
+import { CounterButtonComponent } from './counter/counter-button/counter-button.component';
+import { CounterOutputComponent } from './counter/counter-output/counter-output.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CounterComponent,
+    CounterButtonComponent,
+    CounterOutputComponent
   ],
   imports: [
     BrowserModule
