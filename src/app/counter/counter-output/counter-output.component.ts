57ffebdf53bdf6fab67782380beef1d64f2febc8
Pratik Kumar <pratik.kumar@mobileprogramming.com>
2023-06-23 18:22:19 +0530

    counter as observable

--- src/app/counter/counter-output/counter-output.component.ts
+++ src/app/counter/counter-output/counter-output.component.ts
@@ -1,29 +1,31 @@
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { CounterState } from 'src/app/counter/state/counter.state';
-import { Subscription } from 'rxjs';
+import { Subscription, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-counter-output',
   templateUrl: './counter-output.component.html',
   styleUrls: ['./counter-output.component.css']
 })
-export class CounterOutputComponent implements OnInit, OnDestroy {
+export class CounterOutputComponent implements OnInit {
   // @Input() counter: any;
-  counter!: number
-  counterSubscription!: Subscription
+  // counter!: number
+  counter$!: Observable<CounterState>
+  // counterSubscription!: Subscription
 
   constructor(private store: Store<{ counter: CounterState }>) { }
 
   ngOnInit(): void {
-    this.counterSubscription = this.store.select('counter')
-      .subscribe(data => {
-        this.counter = data.counter
-      })
+    // this.counterSubscription = this.store.select('counter')
+    //   .subscribe(data => {
+    //     this.counter = data.counter
+    //   })
+    this.counter$ = this.store.select('counter')
   }
 
-  ngOnDestroy(): void {
-    this.counterSubscription.unsubscribe()
-  }
+  // ngOnDestroy(): void {
+  //   this.counterSubscription.unsubscribe()
+  // }
 
 }
